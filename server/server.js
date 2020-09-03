52a48b0bac6b2ed09804c56862a072b82ff377e1
japerry911 <jack.e.perry@outlook.com>
2020-09-03 16:22:58 -0600

    graphql and express merged together

--- server/server.js
+++ server/server.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const http = require("http");
 require("dotenv").config();
 
 const app = express();
+
+const typeDefs = `
+  type Query {
+    totalPosts: Int!
+  }  
+`;
+
+const resolvers = {
+  Query: {
+    totalPosts: () => 42,
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+apolloServer.applyMiddleware({ app });
+
+const httpServer = http.createServer(app);
 
 app.get("/rest", (req, res) => {
   res.json({
