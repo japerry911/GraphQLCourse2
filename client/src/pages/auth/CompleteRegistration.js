0dea65cb6a8a798c2a32a022c6bc69832ec6873f
japerry911 <jack.e.perry@outlook.com>
2020-09-07 06:46:58 -0600

    progress on complete registration, added in basic full authentication cycle

--- client/src/pages/auth/CompleteRegistration.js
+++ client/src/pages/auth/CompleteRegistration.js
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { auth } from "../../firebase";
 import { toast } from "react-toastify";
 import { useHistory } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
 
 const CompleteRegistration = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+
+  const { dispatch } = useContext(AuthContext);
 
   const history = useHistory();
 
@@ -14,14 +17,49 @@ const CompleteRegistration = () => {
     setEmail(window.localStorage.getItem("emailForRegistration"));
   }, [history]);
 
-  const handleSubmit = (event) => {};
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setLoading(true);
+
+    if (!email || !password) {
+      toast.error("Email and password is required");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const result = await auth.signInWithEmailLink(
+        email,
+        window.location.href
+      );
+
+      if (result.user.emailVerified) {
+        window.localStorage.removeItem("emailForRegstration");
+        const user = auth.currentUser;
+        await user.updatePassword(password);
+
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: { email: user.email, token: idTokenResult.token },
+        });
+
+        history.push("/");
+      }
+    } catch (error) {
+      console.log("register complete error", error.message);
+      setLoading(false);
+      toast.error(error.message);
+    }
+  };
 
   return (
     <div className="container p-5">
       {loading ? (
         <h4 className="text-danger">Loading...</h4>
       ) : (
-        <h4>Register</h4>
+        <h4>Complete Registration</h4>
       )}
       <form onSubmit={handleSubmit}>
         <div className="form-group">
