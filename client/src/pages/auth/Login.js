7c004819d2b87d8ffd0f1e87867cf0c81460c50e
japerry911 <jack.e.perry@outlook.com>
2020-09-07 10:44:29 -0600

    sign out and sign in functionality added, also added conditions for navbar tabs

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,10 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+import { Link, useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+import { auth, googleAuthProvider } from "../../firebase";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
 
-  const handleSubmit = () => {};
+  const { dispatch } = useContext(AuthContext);
+
+  const history = useHistory();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setLoading(true);
+
+    try {
+      const result = await auth.signInWithEmailAndPassword(email, password);
+
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+
+      dispatch({
+        type: "LOGGED_IN_USER",
+        payload: { email: user.email, token: idTokenResult.token },
+      });
+
+      history.push("/");
+    } catch (error) {
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="container p-5">
@@ -21,6 +52,23 @@ const Login = () => {
             disabled={loading}
           />
         </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+            placeholder="Enter Password"
+            disabled={loading}
+          />
+        </div>
+        <button
+          className="btn btn-raised btn-primary"
+          disabled={!password || loading || !email}
+        >
+          Sign In
+        </button>
       </form>
     </div>
   );
