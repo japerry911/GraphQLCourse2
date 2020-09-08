b4020de78e91b866af067e74ffcb54bc660079a9
japerry911 <jack.e.perry@outlook.com>
2020-09-07 19:15:59 -0600

    progress on userCreate mutation

--- server/resolvers/auth.js
+++ server/resolvers/auth.js
@@ -1,13 +1,29 @@
 const { gql } = require("apollo-server-express");
 const { authCheck } = require("../helpers/auth");
+const User = require("../models/user");
+const shortid = require("shortid");
 
 const me = async (parentValue, args, { req, res }) => {
   await authCheck(req);
   return "Skylord";
 };
 
+const userCreate = async (parentValue, args, { req }) => {
+  const currentUser = await authCheck(req);
+  const user = await User.findOne({ email: currentUser.email });
+  return user
+    ? user
+    : new User({
+        email: currentUser.email,
+        username: shortid.generate(),
+      }).save();
+};
+
 module.exports = {
   Query: {
     me,
+  },
+  Mutation: {
+    userCreate,
   },
 };
